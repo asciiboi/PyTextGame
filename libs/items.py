1e834b48c71a06cb68e0704bd89924ee7a929e7e
slasken06 <slasken2006@gmail.com>
2021-09-01 15:28:45 +0200

    Setup of chests items and loottables. Setup of .gitignore. Setup of main.py

---
+++ libs/items.py
@@ -0,0 +1,15 @@
+class Chest:
+    def __init__(self, loottable):
+        self.loottable = loottable
+
+class loottable:
+    def __init__(self):
+        self.table = []
+
+    def add(item):
+        self.table.append(item)
+
+class item:
+    def __init__(self, name, ItemId):
+        self.name = name
+        self.id = ItemId
